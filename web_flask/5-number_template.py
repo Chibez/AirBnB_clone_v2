1eb8844167afc28bd7f057ccfb673ff6f1a573af
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2024-04-20 16:40:50 -0700

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
+"""Flask"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Display 'Hello HBNB!'"""
+    """Return a given string"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Display 'HBNB'"""
+    """Returns a given string"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """Display 'C ' followed by the value of the text variable"""
+def cText(text):
+    """display C followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    """Display 'Python ' followed by the value of the text variable"""
+def pythonText(text="is cool"):
+    """display Python followed by the value of the text variable"""
     return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def is_number(n):
-    """Display 'n is a number' only if n is an integer"""
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
     if isinstance(n, int):
         return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Display HTML page with 'Number: n' in H1 tag"""
+def number_template(n=None):
+    """display a HTML page only if n is an integer"""
     if isinstance(n, int):
-        return render_template('number_template.html', n=n)
+        return render_template("5-number.html", n=n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run()
